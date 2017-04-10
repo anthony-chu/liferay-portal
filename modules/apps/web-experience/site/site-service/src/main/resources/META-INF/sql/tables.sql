cbe2ae0d0d718f5c6f5ec4ad7f126fe30010e85f
Anthony Chu <anthony.chu@liferay.com>
2017-04-10 15:27:01 -0700

    Revert "LPS-71615 regenerate service code"
    This reverts commit c7dd2ebd7732727cf04ab6b8213e6665c0f42a72.

--- modules/apps/web-experience/site/site-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/web-experience/site/site-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,6 +1,21 @@
-create table SiteFriendlyURL (
+create table GroupFriendlyURL (
 	uuid_ VARCHAR(75) null,
-	siteFriendlyURLId LONG not null primary key,
+	groupFriendlyURLId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	groupId LONG,
+	friendlyURL VARCHAR(75) null,
+	languageId VARCHAR(75) null,
+	lastPublishDate DATE null
+);
+
+create table Site_GroupFriendlyURL (
+	mvccVersion LONG default 0 not null,
+	uuid_ VARCHAR(75) null,
+	groupFriendlyURLId LONG not null primary key,
 	companyId LONG,
 	userId LONG,
 	userName VARCHAR(75) null,
