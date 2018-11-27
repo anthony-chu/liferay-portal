66cdc04e5729e22f9f15b1672f752fb50655417a
Anthony Chu <anthony.chu@liferay.com>
2018-11-27 09:18:02 -0800

    Revert "LPS-87055 Use new method"
    This reverts commit 1c9301108ccf52515a52a550fd04cb95bd4803cc.

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/model/listener/LayoutPrototypeModelListener.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/model/listener/LayoutPrototypeModelListener.java
@@ -102,49 +102,36 @@ public class LayoutPrototypeModelListener
 	public void onAfterUpdate(LayoutPrototype layoutPrototype)
 		throws ModelListenerException {
 
-		try {
-			LayoutPageTemplateEntry layoutPageTemplateEntry =
-				_layoutPageTemplateEntryLocalService.
-					fetchFirstLayoutPageTemplateEntry(
-						layoutPrototype.getLayoutPrototypeId());
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.
+				fetchFirstLayoutPageTemplateEntry(
+					layoutPrototype.getLayoutPrototypeId());
 
-			if (layoutPageTemplateEntry == null) {
-				return;
-			}
-
-			String nameXML = layoutPrototype.getName();
-
-			Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
-				nameXML);
-
-			Locale defaultLocale = LocaleUtil.fromLanguageId(
-				LocalizationUtil.getDefaultLanguageId(nameXML));
-
-			String name = nameMap.get(defaultLocale);
-
-			int status;
-
-			if (layoutPrototype.isActive()) {
-				status = WorkflowConstants.STATUS_APPROVED;
-			}
-			else {
-				status = WorkflowConstants.STATUS_INACTIVE;
-			}
-
-			long layoutPageTemplateEntryId =
-				layoutPageTemplateEntry.getLayoutPageTemplateEntryId();
-			long userId = layoutPageTemplateEntry.getUserId();
-
-			_layoutPageTemplateEntryLocalService.updateLayoutPageTemplateEntry(
-				userId, layoutPageTemplateEntryId, name, status);
+		if (layoutPageTemplateEntry == null) {
+			return;
 		}
-		catch (PortalException pe) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(pe, pe);
-			}
 
-			throw new ModelListenerException(pe);
+		String nameXML = layoutPrototype.getName();
+
+		Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
+			nameXML);
+
+		Locale defaultLocale = LocaleUtil.fromLanguageId(
+			LocalizationUtil.getDefaultLanguageId(nameXML));
+
+		layoutPageTemplateEntry.setName(nameMap.get(defaultLocale));
+
+		if (layoutPrototype.isActive()) {
+			layoutPageTemplateEntry.setStatus(
+				WorkflowConstants.STATUS_APPROVED);
 		}
+		else {
+			layoutPageTemplateEntry.setStatus(
+				WorkflowConstants.STATUS_INACTIVE);
+		}
+
+		_layoutPageTemplateEntryLocalService.updateLayoutPageTemplateEntry(
+			layoutPageTemplateEntry);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
