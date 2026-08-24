67b87c5bc1065388eda8adabf666eda68d8211c5
Anthony Chu <anthony.chu@liferay.com>
2026-08-24 08:56:01 -0700

    LPD-96495 Rename for clarity

--- workspaces/liferay-sample-workspace/lmnr-evals/create-event-registration.eval.ts
+++ workspaces/liferay-sample-workspace/lmnr-evals/create-event-registration.eval.ts
@@ -20,7 +20,7 @@ const data = [
     }
 ];
 
-const OBJECT_REPORT_SCHEMA = {
+const STRUCTURED_OUTPUT_SCHEMA = {
     additionalProperties: false,
     properties: {
         objects: {
@@ -117,5 +117,5 @@ evaluate({
         "Skills invoked": skillsInvokedEvaluator,
         "Objects created": objectsCreatedEvaluator,
     },
-    executor: createAgentTask(OBJECT_REPORT_SCHEMA),
+    executor: createAgentTask(STRUCTURED_OUTPUT_SCHEMA),
 });
