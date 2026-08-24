021276105143624f5f889274225cd6f903736e06
Anthony Chu <anthony.chu@liferay.com>
2026-08-24 15:57:44 -0700

    LPD-96495 More stable check

--- workspaces/liferay-sample-workspace/lmnr-evals/lib/evaluators.ts
+++ workspaces/liferay-sample-workspace/lmnr-evals/lib/evaluators.ts
@@ -1,19 +1,7 @@
 export const skillsInvokedEvaluator = (output, target) => {
-    const actualSkillsInvoked = [...output.skillsInvoked];
-    const expectedSkillsInvoked = [...target.skillsInvoked];
+    const actualSkillsInvoked = new Set(output.skillsInvoked);
 
-    if (actualSkillsInvoked.length !== expectedSkillsInvoked.length) {
-        return 0.0;
-    }
-
-    actualSkillsInvoked.sort();
-    expectedSkillsInvoked.sort();
-
-    for (let i = 0; i < expectedSkillsInvoked.length; i++) {
-        if (expectedSkillsInvoked[i] !== actualSkillsInvoked[i]) {
-            return 0.0;
-        }
-    }
-
-    return 1.0;
+    return target.skillsInvoked.every((skill) => actualSkillsInvoked.has(skill))
+        ? 1.0
+        : 0.0;
 };
