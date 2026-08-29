5bdc1dcbff10f88f8d582b2328dc69474b81cad0
Anthony Chu <anthony.chu@liferay.com>
2026-08-28 17:10:44 -0700

    LPD-96495 Add configs to eval

--- workspaces/liferay-sample-workspace/langfuse-evals/lib/otel.ts
+++ workspaces/liferay-sample-workspace/langfuse-evals/lib/otel.ts
@@ -1,9 +1,12 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { isDefaultExportSpan, LangfuseSpanProcessor } from "@langfuse/otel";
 
+import { LANGFUSE_CONFIG } from "./langfuse.ts";
+
 export const otelSdk = new NodeSDK({
     spanProcessors: [
         new LangfuseSpanProcessor({
+            ...LANGFUSE_CONFIG,
             shouldExportSpan: ({ otelSpan }) =>
                 otelSpan.instrumentationScope.name.startsWith("@arizeai/openinference")
                     || isDefaultExportSpan(otelSpan),
