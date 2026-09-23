b74894130a880bdc8dafa8364d6006f5e8b355bb
Kyle Miho <kyle.miho@liferay.com>
2026-09-23 10:29:43 -0700

    LPD-103392 Add deployables from 2nd eval

--- workspaces/liferay-sample-workspace/client-extensions/devcon-site-initializer/site-initializer/fragments/group/devcon/fragments/event-card/index.js
+++
@@ -1,2 +0,0 @@
-/* Event Card — content is populated either by editable region defaults or, inside a Collection, by
- * fragmentFields mapping to Event object fields (see the Collection element in page-definition.json). */
